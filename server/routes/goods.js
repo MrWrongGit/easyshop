32a819321c20bf87602fd59853a18c96dc5771bc
MrWrongGit <846047920@qq.com>
2018-04-10 18:58:04 +0800

    modify server

--- server/routes/goods.js
+++ server/routes/goods.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 var model = require('../models/base')
-var Owner = model.Owner
 var Shop = model.Shop
 var Goods = model.Goods
 
@@ -11,69 +10,66 @@ var multer  = require('multer')
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, 'public/images/'+req.params.s_id +'/'+req.params.g_id+'/')
+      cb(null, 'public/images/')
     },
     filename: function (req, file, cb) {
-      cb(null, file.fieldname+"-"+Date.now()+path.extname(file.originalname))
+        var filename = file.fieldname+"-"+req.params.g_id
+        if(req.url.split('/')[1]==="banner"){
+            filename += "-"+Date.now()
+        }
+        cb(null, filename+path.extname(file.originalname))
     }
 })
 
 var upload = multer({ storage: storage })
 
-router.post('/thumbnail/:g_id/:s_id', upload.single('thumbnail'), function(req, res, next) {
-    
-    console.log(req.file);
-    res.json({
-        status:'ok'
-    }
-  )
-});
-
-router.post('/banner/:g_id/:s_id', upload.single('banner'), function(req, res, next) {
-    
-    console.log(req.file);
-    res.json({
-        status:'ok'
-    }
-  )
-});
-
-router.get('/test1', function(req, res, next) {
-    
-    Owner.create({
-        name:'zhl', 
-        tel:'15072357587',
-        addr:'123',
-        date:'2018-01-01',
-        pswd:'5811430'
-    },(err,doc)=>{
-        console.log(doc)
+/////////////////////////////
+router.post('/thumbnail/:g_id/', upload.single('thumbnail'), function(req, res, next) {
+    Goods.update({_id: req.params.g_id},{thumbnail:req.file.filename},(err, doc)=>{
+        if(err!==null){
+            res.json({status:"fail",errMsg:"数据库错误!"})
+        }else{
+            res.json({status:'ok',thumbnail:"images/"+req.file.filename})
+        }
     })
-
-    res.json({
-        status:'ok'
-    }
-  )
 });
+
+/////////////////////////////
+router.post('/banner/:g_id/', upload.single('banner'), function(req, res, next) {
+    Goods.update({_id: req.params.g_id},
+        {'$push': {banner: req.file.filename}},
+        (err,doc)=>{
+            if(err!==null){
+                res.json({status:"fail",errMsg:"数据库错误!"})
+            }else{
+                res.json({status:'ok',banner:"images/"+req.file.filename})
+            }
+    })
+});
+
 
 ////////////////////////////////////
 router.post('/basicinfo/:s_id', function(req, res, next) {
-    
     Goods.create({
         basic: {...req.body.basic}
     },(err,doc)=>{
-        console.log(err)
-        console.log(doc)
-
-        Shop.update({_id: req.params.s_id},
-            {'$push': {goodsIdList: doc._id}},
-            (err, doc)=>{
-                res.json({
-                    status:'ok'
-                }
-        )
+        if(err!==null){
+            res.json({status:"fail",errMsg:"数据库错误!"})
+        }else{
+            var goodsId = doc._id
+            Shop.update({_id: req.params.s_id},
+                {'$push': {goodsIdList: doc._id}},
+                (err, doc)=>{
+                    console.log(doc)
+                    if(err!==null){
+                        Goods.remove({_id: goodsId}, (err, doc)=>{})
+                        res.json({status:"fail",errMsg:"数据库错误!"})
+                    }else{
+                        res.json({status:"ok","goodsId":goodsId})
+                    }
+            })
+        }
     })
-})});
-
+});
 
 module.exports = router;
--- server/routes/shop.js
+++ server/routes/shop.js
@@ -13,10 +13,9 @@ router.post('/', function(req, res, next) {
     //creat owner item in owner-collection
     Owner.create({...req.body.owner,date:Date.now()},(err,doc)=>{
         if(err!==null){
-            res.json({status:"fail",errMsg:"数据库错误1!"})
+            res.json({status:"fail",errMsg:"数据库错误!"})
         }else{
             ownerId = doc._id
-
             Shop.create({
                 name: req.body.name,
                 ownerInfoId: ownerId
@@ -24,9 +23,7 @@ router.post('/', function(req, res, next) {
                 if(err!==null){
                     //delete owner item
                     Owner.remove({_id: ownerId}, (err, doc)=>{})
-
                     res.json({status:"fail",errMsg:"数据库错误!"})
-                    console.log(err)
                 }else{
                     res.json({status:"ok",shopId:doc._id})
                 }
